0abfdde896e5b53674b995b68e7fde06e90c6dcf
Winfred <winfrednginakilonzo@gmail.com>
2023-02-17 08:06:32 -0800

    lower_and_upper_case

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,10 +10,13 @@ int  main(void)
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
+	{
 		putchar(letter);
-
-	for (letter = 'A'; letter <= 'z'; letter++)
+	}
+	for (letter = 'A'; letter <= 'Z'; letter++)
+	{
 		putchar(letter);
+	}
 	putchar('\n');
 	return (0);
 }
