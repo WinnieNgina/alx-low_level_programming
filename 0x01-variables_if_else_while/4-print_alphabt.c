87e83821fd54329d623e85815f8ac4e980048c7a
Winfred <winfrednginakilonzo@gmail.com>
2023-02-17 08:25:08 -0800

    ommission

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - print all the alphabets in lower case except for q and e
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'e' && letter != 'q')
+			putchar(letter);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
