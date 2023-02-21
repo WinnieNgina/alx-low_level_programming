a826703b64a2de242c28c4b7e63e322deacf426d
Winfred <winfrednginakilonzo@gmail.com>
2023-02-21 03:36:45 -0800

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,12 +4,11 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void)
+void print_alphabet(void)
 {
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
 		_putchar(letter);
 	_putchar('\n');
-	return (0);
 }
