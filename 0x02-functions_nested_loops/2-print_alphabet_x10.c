2f2357e892bf018d851db47999528b14060d24db
Winfred <winfrednginakilonzo@gmail.com>
2023-02-21 04:33:41 -0800

    10_times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - prints all alphabets in lower case for 10 times. 
+ * print_alphabet_x10 - prints all alphabets in lower case for 10 times
  */
 void print_alphabet_x10(void)
 {
