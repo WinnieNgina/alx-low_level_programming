a75133ea952d7d42b1f5a0f8b1c3a104fefb9e09
Winfred <winfrednginakilonzo@gmail.com>
2023-02-21 02:55:59 -0800

    alpabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = a; letter <= z; letter++)
+		_putchar(letter);
+	_putchar('\n');
+	return (0);
+}
