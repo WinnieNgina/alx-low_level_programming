77b6026ed13dd37bdd63598f051c07173e43dfed
Winfred <winfrednginakilonzo@gmail.com>
2023-03-01 13:34:23 -0800

    comparing

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -13,7 +13,10 @@ int _strcmp(char *s1, char *s2)
 	for (i = 0; s1[i] != '\0'; i++)
 	{
 	for (j = 0; s2[j] != '\0'; j++)
-		return (s1[i] - s2[j]);
+	{
+		if (if (s1[i] != s2[j])
+				return (s1[i] - s2[j]);
+	}
 	}
 	return (0);
 }
