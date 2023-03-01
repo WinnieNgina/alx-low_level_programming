2fd0e98cfeadd62c26bb2c6ba7212f8c4e6a87ec
Winfred <winfrednginakilonzo@gmail.com>
2023-03-01 13:26:13 -0800

    comparing

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strcmp - compares two strings
+ * @s1: String one
+ * @s2: string two
+ *
+ * Return: (s1 - s2)
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i, j;
+
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+	for (j = 0; s2[j] != '\0'; j++)
+		return (s1[i] - s2[j]);
+	}
+	return(0);
+}
