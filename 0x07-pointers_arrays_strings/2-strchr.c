6fbe3563607cdbde83c5e1095993ce5e0c2f4c8c
Winfred <winfrednginakilonzo@gmail.com>
2023-03-06 05:55:54 -0800

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,15 +9,9 @@ char *_strchr(char *s, char c)
 {
 	int i;
 
-	for (i = 0; i < '\0'; i++)
-	{
-		if (s[i] == c)
-		return (s);
-	}
-	for (i = 0; i < '\0'; i++)
-	{
-		if (s[i] != c)
-			return ('\0');
-	}
-	return (0);
+	for (i = 0; (s[i] != c) && (s[i] != '\0'); i++);
+	if (s[i] == c)
+		return (s + i);
+	else
+		return ('\0');
 }
