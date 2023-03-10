256be2008f996825d527b2a65e0e24f0a0b2b3d1
Winfred <winfrednginakilonzo@gmail.com>
2023-03-10 09:58:07 -0800

    print string

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints all arguments it receives
+ * @argc: number/count of arguments
+ * @argv: argument vector
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 1; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
