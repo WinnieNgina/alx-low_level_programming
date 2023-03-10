42d9c1815789cbf14ff6434a98996a64ca0cc331
Winfred <winfrednginakilonzo@gmail.com>
2023-03-10 07:48:33 -0800

    number of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - prints the program name followed by new line
+ * @argc: number/count of arguments
+ * @argv: argument vector
+ * Return: 0
+ */
+int main(int argc, char __attribute__((__unused__)) *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
