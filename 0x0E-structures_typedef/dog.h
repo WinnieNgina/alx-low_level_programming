434a3b890c097e4392875b47f84c27abfa934823
Winfred <winfrednginakilonzo@gmail.com>
2023-03-20 13:21:33 -0700

    struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,11 @@
 #ifndef DOG_H
 #define DOG_H
-
+/**
+ * struct dog - structure for printing dog details
+ * @name: name of the dog
+ * @age: Age of the dog
+ * @owner: Dog owner name
+ */
 struct dog
 {
 char *name;
