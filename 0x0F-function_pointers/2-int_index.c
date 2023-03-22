aa185e1691211247fc7f24b24d14051044344a3c
Winfred <winfrednginakilonzo@gmail.com>
2023-03-22 07:14:59 -0700

    identify integers

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,18 +12,14 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (array != NULL && cmp != NULL)
+	if (size < 1 || array == NULL || cmp == NULL)
+		return (-1);
+
+	for (i = 0; i < size; i++)
 	{
-		if (size <= 0)
-			return (-1);
+		if (cmp(array[i]))
+			return (i);
 	}
-		for (i = 0; i < size; i++)
-		{
-			if (cmp(array[i]) != 0)
-			{
-				return (i);
-			}
-		}
+
 	return (-1);
 }
-
