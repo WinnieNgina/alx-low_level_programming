748b91016679300dc7848cd8a0e97a29e0ffdc77
Winfred <winfrednginakilonzo@gmail.com>
2023-03-23 10:43:01 -0700

    printing_numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -20,9 +20,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		{
 			printf("%d", va_arg(args, int));
 			if (i != (n - 1))
-			{
 				printf("%s", separator);
-			}
 		}
 	}
 	printf("\n");
