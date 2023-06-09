dc96e17668881ad4a315db7f07da9fd6c7d052fe
Winfred <winfrednginakilonzo@gmail.com>
2023-06-09 09:12:26 -0700

    add comments

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -4,28 +4,26 @@
 
 /**
  * hash_table_get - Retrieves the value associated with a key in the hash table
- * @ht: The hash table to look into
- * @key: The key to search for
+ * @ht: The hash table we are retriving the key from
+ * @key: The key to retrive
  *
- * Return: The value associated with the element, or NULL if key couldn't be found
+ * Return: The value associated with the key, or NULL if key couldn't be found
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-    hash_node_t *node;
-    unsigned long int index;
+	hash_node_t *node;
+	unsigned long int index;
 
-    if (ht == NULL || key == NULL || *key == '\0')
-        return NULL;
-
-    index = key_index((const unsigned char *)key, ht->size);
-    node = ht->array[index];
-
-    while (node != NULL)
-    {
-        if (strcmp(node->key, key) == 0)
-            return node->value;
-        node = node->next;
-    }
-
-    return NULL;
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
+	index = key_index((const unsigned char *)key, ht->size);
+	node = ht->array[index];
+	/*helps us iterate through the hash table*/
+	while (node != NULL)
+	{
+		if (strcmp(node->key, key) == 0)
+			return (node->value);
+		node = node->next;
+	}
+	return (NULL);
 }
