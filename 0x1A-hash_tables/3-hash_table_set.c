ca156fbd6e6f8f8c316f50ee8548ab2082de3e6e
Winfred <winfrednginakilonzo@gmail.com>
2023-06-09 08:24:47 -0700

    remove add function

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,92 +1,63 @@
 #include "hash_tables.h"
 
 /**
- * addNode - Adds a new node to the beginning of a linked list.
- * @head: Pointer to the head of the linked list.
- * @key: Key for the new node.
- * @value: Value for the new node.
- */
-void addNode(hash_node_t **head, const char *key, const char *value)
-{
-	/* Create a new node and allocate memory*/
-	hash_node_t *newNode = (hash_node_t *)malloc(sizeof(hash_node_t));
-
-	newNode->key = strdup(key);
-	newNode->value = strdup(value);
-	/* Make the new node point to the current head */
-	newNode->next = *head;
-	/* Update the head to point to the new node */
-	*head = newNode;
-}
-/**
- * create_item - Creates a new hash node with the given key and value.
- * @key: Key for the new hash node.
- * @value: Value for the new hash node.
- * Return: Pointer to the newly created hash node, or NULL if an error occurs.
- */
+* create_item - Creates a new hash node with the given key and value.
+* @key: Key for the new hash node.
+* @value: Value for the new hash node.
+*
+* Return: Pointer to the newly created hash node, or NULL if an error occurs.
+*/
 hash_node_t *create_item(const char *key, const char *value)
 {
-	/* Creates a linked list with our new element */
-	hash_node_t *new_element;
+	hash_node_t *new_item = malloc(sizeof(hash_node_t));
 
-	if (strcmp(key, "") == 0)
-	{
-		/* string cannot be empty */
+	if (!new_item)
 		return (NULL);
-	}
-	new_element = malloc(sizeof(hash_node_t));
-	/* Mem allocation check */
-	if (new_element == NULL)
-	{
-		return (NULL);
-	}
-	/* Mem allocation for members of new_element */
-	new_element->key = strdup(key);
-	new_element->value = strdup(value);
-	new_element->next = NULL;
-	return (new_element);
+
+	new_item->key = strdup(key);
+	new_item->value = strdup(value);
+	new_item->next = NULL;
+
+	return (new_item);
 }
 
 /**
- * hash_table_set - Inserts or updates an element in the hash table.
- * @ht: Pointer to the hash table.
- * @key: Key for the element.
- * @value: Value for the element.
- *
- * Return: 1 on success, 0 on failure.
- */
+* hash_table_set - Inserts or updates an element in the hash table.
+* @ht: Pointer to the hash table.
+* @key: Key for the element.
+* @value: Value for the element.
+*
+* Return: 1 on success, 0 on failure.
+*/
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *new_element;
 	unsigned long int index;
-	hash_node_t *current_element;
+	hash_node_t *new_node, *current_node;
 
-	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+	if (!ht || !key || !value)
+		return (0);
+
+	index = key_index((unsigned char *)key, ht->size);
+
+	/* // Check if the key already exists and update the value if so */
+	current_node = ht->array[index];
+	while (current_node)
 	{
-		return (0); /*Invalid input, return failure*/
+		if (strcmp(current_node->key, key) == 0)
+		{
+		free(current_node->value);
+		current_node->value = strdup(value);
+		return (1);
+		}
+		current_node = current_node->next;
 	}
-	/* Create new element */
-	new_element = create_item(key, value);
-	/* Generate hash code for key & Generate key index for the key */
-	index = key_index((const unsigned char *)key, ht->size);
-	/* Store current index of array in a variable */
-	current_element = ht->array[index];
-	if (new_element == NULL)
-	{
-		return (0); /*Failed to create new element, return failure*/
-	}
-	/* Check if index in array is NULL */
-	if (current_element == NULL)
-	{
-		/* If index is NULL, insert element (create linked list) */
-		ht->array[index] = new_element;
-		return (1); /* Return success*/
-	}
-	else
-	{
-		/* If index is not NULL, handle collision */
-		addNode(&ht->array[index], key, value);
-		return (1); /* Return success*/
-	}
+
+	/* // Create a new node and add it at the beginning of the list */
+	new_node = create_item(key, value);
+	if (!new_node)
+		return (0);
+
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+	return (1);
 }
-
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -41,7 +41,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
-void addNode(hash_node_t **head, const char *key, const char *value);
 hash_node_t *create_item(const char *key, const char *value);
 
 #endif
