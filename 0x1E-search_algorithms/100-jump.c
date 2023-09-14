40f31af992def76712b9db4a96ec3285ceab3e62
Winfred <winfrednginakilonzo@gmail.com>
2023-09-14 21:02:56 +0300

    fix jump search

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,57 +1,38 @@
-#include<stdio.h>
-#include<math.h>
+#include <stdio.h>
+#include <math.h>
 /**
- * mini - finds the minimum value
- * @a: number compared
- * @b: number compared with
- * Return: The minimum integer
- */
-size_t mini(size_t a, size_t b)
-{
-	if (b > a)
-		return (a);
-	else
-		return (b);
-}
-/**
- * jump_search - searches for a value in a sorted array of integers 
- * @array: array we are searchin the element in
+ * jump_search - searches for a value in a sorted array of integers
+ * @array: array we are searching the element in
  * @size: size of the array
  * @value: value we are searching
- * Return: The index of the value or -1 if array is empty
+ * Return: The index of the value or -1 if the array is empty
  */
 int jump_search(int *array, size_t size, int value)
 {
 	size_t skip;
 	size_t prev;
-	int index;
+	size_t index;
 
-	if (array == NULL)
+	if (array == NULL || size == 0)
 	{
 		return (-1);
 	}
 	skip = sqrt(size);
-	index = mini(skip, size) - 1;
-	while (array[index] < value)
+	index = prev = 0;
+	while (prev < size && array[prev] < value)
 	{
-		prev = skip;
-		skip += sqrt(size);
-		if (prev >= size)
-		{
-			return (-1);
-		}
+		printf("Value checked array[%ld] = [%d]\n", prev, array[prev]);
+		index = prev;
+		prev += skip;
 	}
-	while (array[prev] < value)
+	printf("Value found between indexes [%ld] and [%ld]\n", index, prev);
+	prev = prev < size - 1 ? prev : size - 1;
+	while (index < prev && array[index] < value)
 	{
-		prev++;
-		if (prev == mini(skip, size))
-		{
-			return (-1);
-		}
+		printf("Value checked array[%ld] = [%d]\n", index, array[index]);
+		index++;
 	}
-	if (array[prev] == value)
-	{
-		return (prev);
-	}
-	return (-1);
+	printf("Value checked array[%ld] = [%d]\n", index, array[index]);
+	return (array[index] == value ? (int)index : -1);
+
 }
